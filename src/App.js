ff25643463fee06c27311e8445b7d43cf9c63846
Carlos Moreno <carlosmorenodominguez1@hotmail.com>
2021-11-05 03:31:45 -0600

    first push

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-
+import styled from 'styled-components';
+import "./app.css";
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -8,6 +9,10 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+export default function App()
+
+
+
 
   return (
     <div className="App">
@@ -15,5 +20,9 @@ const App = () => {
     </div>
   );
 }
+const WebTitle = styled.h1`
+font-size: 3rem;
+color: blue;
 
+`
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,13 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+
+
+
+
+export function Character(props) {
+    return
+
+
+}
